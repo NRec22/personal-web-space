b18ac7f905345fd18a8226a74c4c87278a32a8e0
Neven Recchia <nevenrecchia@gmail.com>
2018-06-03 19:39:19 -0400

    add eslint and basic react component

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// Author: Neven Recchia
-console.info('Hello, world!');
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+// Author: Neven Recchia
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+console.info('Hello, world!');
