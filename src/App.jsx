1782f2241835e2938557fe1f2928a3775b62387b
Neven Recchia <nevenrecchia@gmail.com>
2018-06-05 00:38:38 -0400

    fix: apply eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    );
-  }
-}
+const App = () => <h1>Hello, world!</h1>;
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 // Author: Neven Recchia
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx'
+import App from './App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
 
+/* eslint-disable no-console*/
 console.info('Hello, world!');
